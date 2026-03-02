3611b494e0d94408151d87a5d3db61a2d86135ea
fifa08865 <164646367+fifa08865@users.noreply.github.com>
2026-03-03 01:33:00 +0700

    Delete ConfigUDD.lua

--- ConfigUDD.lua
+++
@@ -1,8 +0,0 @@
-local ply = game:GetService("Players").LocalPlayer
-local messages = "💎 Gems :  "
-local Gem = tostring(ply:GetAttribute("Gems"))
-
-print(Gem)
-while task.wait(1) do
-    _G.Horst_SetDescription(messages..Gem)
-end
